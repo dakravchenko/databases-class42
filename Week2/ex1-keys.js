fe0e286608b41f1f526e2cd52e912eb2379623a8
Daniil <daniil.kravchenko.92@yandex.ru>
2023-03-28 17:10:03 +0200

    ex2 upd

--- Week2/ex1-keys.js
+++ Week2/ex1-keys.js
@@ -13,56 +13,68 @@ db.connect(err => {
   console.log('MySql connected...');
 });
 
-
 db.query('CREATE DATABASE IF NOT EXISTS week2db');
 
 db.query('USE week2db', err => {
-    if (err) {
-      throw err;
-    }
-  });
+  if (err) {
+    throw err;
+  }
+});
 
 db.query('DROP TABLE IF EXISTS authors');
 
 db.query(
-    'CREATE TABLE IF NOT EXISTS authors (author_id INT AUTO_INCREMENT PRIMARY KEY, author_name VARCHAR(255), university VARCHAR(255), date_of_birth DATE, h_index INT, gender VARCHAR(20))',
-    err => {
-      if (err) {
-        throw err;
-      }
-      console.log('Table Authors Created');
+  'CREATE TABLE IF NOT EXISTS authors (author_id INT AUTO_INCREMENT PRIMARY KEY, author_name VARCHAR(255), university VARCHAR(200), date_of_birth DATE, h_index INT, gender VARCHAR(20))',
+  err => {
+    if (err) {
+      throw err;
     }
-  );
+  }
+);
 
 db.query('ALTER TABLE authors ADD mentor_id INT', err => {
+  if (err) {
+    throw err;
+  }
+});
+
+db.query(
+  'ALTER TABLE authors ADD CONSTRAINT fk_mentor FOREIGN KEY (mentor_id) REFERENCES authors(author_id)',
+  err => {
     if (err) {
       throw err;
     }
-  });
-
-
-db.query(
-    'ALTER TABLE authors ADD CONSTRAINT mentor_id FOREIGN KEY (mentor_id) REFERENCES authors(author_id)',
-    err => {
-      if (err) {
-        throw err;
-      }
-    }
-  );
+  }
+);
 
 db.query(
-    "INSERT INTO authors (author_name, university, date_of_birth, h_index, gender,mentor_id) VALUES ('William Tennesy', 'MIT', '1982-06-14', 10, 'Male', null),('Anna Swiatek', 'MSU', '1988-11-03', 15, 'Female', 1), ('Daniil Medvedev', 'HSE', '1990-02-01', 8, 'Male', 2), ('Claudia Femme', 'SIV', '1987-12-07', 12, 'Female', 1), ('Andrew Kim', 'SU', '1982-06-12', 9, 'Male', 4), ('Emily Wong', 'BU', '1984-08-18', 14, 'Female', 5), ('Chris Lee', 'UC', '1975-03-25', 11, 'Male', 3), ('Karen Davis', 'Yale University', '1993-01-09', 7, 'Female', 7), ('Michael Chen', 'University of Oxford', '1989-11-02', 10, 'Male', 3), ('Rachel Kim', 'University of Oxford', '1981-09-29', 16, 'Female', 2)",
-    err => {
-      if (err) {
-        throw err;
-      }
-      console.log('Author data inserted');
+    `INSERT INTO authors (author_name, university, h_index, mentor_id)
+     VALUES 
+      ('Alice Smith', 'University of ABC', 4, NULL),
+      ('Bob Johnson', 'University of XYZ', 2, 2),
+      ('Charlie Brown', 'University of DEF', 6, 1),
+      ('David Lee', 'University of LMN', 1, NULL),
+      ('Emily Kim', 'University of GHI', 5, 2),
+      ('Frank Liu', 'University of PQR', 3, 8),
+      ('Grace Chen', 'University of JKL', 7, NULL),
+      ('Henry Wong', 'University of UVW', 2, 3),
+      ('Ivy Zhang', 'University of MNO', 4, NULL),
+      ('Jack Lee', 'University of STU', 6, NULL),
+      ('Katie Davis', 'University of RST', 3, 2),
+      ('Larry Chen', 'University of GHI', 1, 1),
+      ('Mary Brown', 'University of DEF', 5, 3),
+      ('Nancy Kim', 'University of JKL', 2, 5),
+      ('Oliver Wu', 'University of ABC', 4, 7);`,
+  err => {
+    if (err) {
+      throw err;
     }
- );
+  }
+);
 
 db.query('SELECT * FROM authors', (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-  });
+  if (err) {
+    throw err;
+  }
+  console.log(result);
+});
