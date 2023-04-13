80aa641db8a9ffdffe0cf2ba2143358745176b89
Daniil <daniil.kravchenko.92@yandex.ru>
2023-04-13 21:45:49 +0200

    week2 hw changes

--- Week2/ex4-aggregate.js
+++ Week2/ex4-aggregate.js
@@ -20,7 +20,14 @@ db.query('USE dbweek2', err => {
 });
 
 db.query(
-  'SELECT research_papers.paper_id, research_papers.paper_title, COUNT(authors.author_id) AS number_of_authors FROM research_papers JOIN author_research_paper ON author_research_paper.research_Paper_id = research_papers.paper_id JOIN authors ON author_research_paper.author_id = authors.author_id GROUP BY research_papers.paper_id ORDER BY research_papers.paper_id',
+`SELECT 
+  research_Papers.paper_id,
+  research_Papers.paper_title,
+  COUNT(author_research_Paper.author_id) AS number_of_authors
+FROM research_Papers 
+JOIN author_research_Paper 
+  ON author_research_Paper.research_Paper_id = research_Papers.paper_id
+GROUP BY research_Papers.paper_id;`,
   (err, result) => {
     if (err) {
       throw err;
@@ -30,7 +37,11 @@ db.query(
 );
 
 db.query(
-  'WITH new_table AS (SELECT authors.author_name, authors.gender, research_papers.paper_id, research_papers.paper_title FROM authors JOIN author_research_paper ON author_research_paper.research_Paper_id = authors.author_id JOIN research_papers ON author_research_paper.author_id = research_papers.paper_id WHERE authors.gender = "Female") SELECT COUNT(gender) AS Sum_of_the_research_papers_published_by_all_female_authors FROM new_table',
+  `SELECT COUNT(authors.gender) AS sum_of_female_works 
+   FROM authors 
+   INNER JOIN author_research_paper ON authors.author_id = author_research_paper.author_id 
+   INNER JOIN research_papers ON author_research_paper.research_Paper_id = research_papers.paper_id 
+   WHERE authors.gender = "Female"`,
   (err, result) => {
     if (err) {
       throw err;
